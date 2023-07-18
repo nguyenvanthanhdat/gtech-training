c38cfc8c8cbcd4a8c98d2a3c13f225d3f37ff3f0
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-07-18 11:09:27 +0700

    Add task 1

---
+++ task1/eli5.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+import torch
+class ELI5(Dataset):
+
+    def __init__(self, json_file_name, json_list):
+        self.json_file_name = json_file_name
+        self.json_list = []
+        with open(json_file_name, 'r') as json_file:
+            json_list = list(json_file)
+
+    def __len__(self):
+        return len(self.json_list)
+
+    def __getitem__(self, index):
+        json = self.json_list[index]
+        json_load = json.loads(json)
+        
+        return json_load

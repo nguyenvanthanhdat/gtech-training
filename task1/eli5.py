9804debf9b974277c2845341b6df866fcab23d0c
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-07-18 12:15:18 +0700

    Add task 1

--- task1/eli5.py
+++ task1/eli5.py
@@ -1,8 +1,9 @@
 from torch.utils.data import Dataset
 import torch
+import json
 class ELI5(Dataset):
 
-    def __init__(self, json_file_name, json_list):
+    def __init__(self, json_file_name):
         self.json_file_name = json_file_name
         self.json_list = []
         with open(json_file_name, 'r') as json_file:

22bd90079cbb78be4b551b193d1392ac68841b91
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-07-20 01:33:14 +0700

    Update task 1

--- task1/main.py
+++ task1/main.py
@@ -1,21 +1,21 @@
 from tqdm import tqdm
-import sys, os, json, argparse
+import sys, os, json, argparse, random
 from torch.utils.data import DataLoader
 from transformers import BartTokenizer, BartForConditionalGeneration
-from eli5 import *
+my_dir = sys.path[0]
+sys.path.append(os.path.join(my_dir, 'src\data'))
+from make_dataset import *
 
 def set_rand():
     SEED = 1234
     random.seed(SEED)
-    np.random.seed(SEED)
+    # np.random.seed(SEED)
     torch.manual_seed(SEED)
     torch.backends.cudnn.deterministic = True
     torch.cuda.is_available()
 
 def main(args):
-
-    my_dir = sys.path[0]
-    sys.path.append(os.path.join(my_dir, '..'))
+    set_rand()
     train_path = 'docs/ELI5.jsonl'
     valid_path = 'docs/ELI5_val.jsonl'
 
@@ -28,9 +28,29 @@ def main(args):
     valid_dataset = ELI5(json_list_valid)
 
     bs = args.batch_size
+    _, question, _, ctxs = train_dataset[0]
+    question_tokens = tokenizer.encode(question,\
+        return_tensors='pt')
+    for doc in ctxs:
+        doc_tokens = tokenizer.encode(doc, return_tensors='pt')
+        combined_tokens = torch.cat((question_tokens, doc_tokens), dim=1)
+        segments = [combined_tokens[:, i:i+max_seq_length]\
+            for i in range(0, combined_tokens.size(1), max_seq_length)]
+        for segment in segments:
+            output = model.generate(
+                segment,
+                max_length=1000,  # Set the maximum length for generated answers
+                num_beams=5,
+                num_return_sequences=1,
+                early_stopping=True
+            )
+            answers.append(tokenizer.decode(output[0]))
 
-    train_data_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True)
-    valid_data_loader = DataLoader(valid_dataset, batch_size=bs, shuffle=False)
+    # train_data_loader = DataLoader(train_dataset, batch_size=bs,\
+    #     shuffle=True)
+
+    valid_data_loader = DataLoader(valid_dataset, batch_size=bs,\
+        shuffle=False)
 
 if __name__ == '__main__':
     import argparse

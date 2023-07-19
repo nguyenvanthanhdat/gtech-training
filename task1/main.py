b29db11918c3ae0f669eb25f925f7bb02b89f89d
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-07-19 20:59:36 +0700

    Update task1

--- task1/main.py
+++ task1/main.py
@@ -4,7 +4,16 @@ from torch.utils.data import DataLoader
 from transformers import BartTokenizer, BartForConditionalGeneration
 from eli5 import *
 
+def set_rand():
+    SEED = 1234
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.cuda.is_available()
+
 def main(args):
+
     my_dir = sys.path[0]
     sys.path.append(os.path.join(my_dir, '..'))
     train_path = 'docs/ELI5.jsonl'
@@ -19,14 +28,15 @@ def main(args):
     valid_dataset = ELI5(json_list_valid)
 
     bs = args.batch_size
-    print(bs)
+
     train_data_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True)
     valid_data_loader = DataLoader(valid_dataset, batch_size=bs, shuffle=False)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', metavar='int', required=True,
-                        help='batch to train model')
+    parser.add_argument('--batch_size', metavar='int',\
+        type=int, required=True,\
+        help='batch to train model')
     args = parser.parse_args()
     main(args=args)
--- task1/main.sh
+++
@@ -1,4 +0,0 @@
-cd task1
-pip install -r requirements.txt
-python main.py --batch_size 1
-pip uninstall -r requirements.txt

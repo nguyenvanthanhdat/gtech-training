49f9ca40488a71a490927dd6cf1f288a71eb2c68
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-07-23 22:28:57 +0700

    change direction save model

--- task1/src/models/train_model.py
+++ task1/src/models/train_model.py
@@ -6,28 +6,30 @@ def train(model, tokenizer, steps, learning_rate, train_data_loader, valid_data_
     loss_function = torch.nn.CrossEntropyLoss()
     max_result_roguel = 0
     step = 0
-    for batch in train_data_loader:
-        print(f'step: {step}')
+    epochs = 10
+    for epoch in epochs:
+        for batch in train_data_loader:
+            print(f'step: {step}')
 
-        input_token = batch[0].input_ids.squeeze()
-        input_mask = batch[0].attention_mask.squeeze()
-        ans_token = batch[1].input_ids.squeeze()
-        loss = model(
-            input_ids = input_token,
-            attention_mask = input_mask,
-            labels=ans_token).loss
-        loss.backward()
+            input_token = batch[0].input_ids.squeeze()
+            input_mask = batch[0].attention_mask.squeeze()
+            ans_token = batch[1].input_ids.squeeze()
+            loss = model(
+                input_ids = input_token,
+                attention_mask = input_mask,
+                labels=ans_token).loss
+            loss.backward()
 
-        optimizer.step()
-        optimizer.zero_grad()
-        
-        if step % 1000 == 0 and step != 0:
-            result_roguel = eval(model, tokenizer, valid_data_loader)
-            if result_roguel > max_result_roguel:
-                torch.save(model,'../models/model.pt')
-                max_result_roguel = result_roguel
-            print(f'{step}: rougeL = {result_roguel}')
-        step += 1
+            optimizer.step()
+            optimizer.zero_grad()
+            
+            if step % 1000 == 0 and step != 0:
+                result_roguel = eval(model, tokenizer, valid_data_loader)
+                if result_roguel > max_result_roguel:
+                    torch.save(model,'/kaggle/working/model.pt')
+                    max_result_roguel = result_roguel
+                print(f'{step}: rougeL = {result_roguel}')
+            step += 1
     result_roguel = eval(model, tokenizer, valid_data_loader)
     if result_roguel > max_result_roguel:
         torch.save(model,'../models/model.pt')

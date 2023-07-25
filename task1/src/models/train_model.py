90f6328ae109629a5813a95c9c4a2f8c5c27b47e
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-07-25 09:36:36 +0700

    Update task 1

--- task1/src/models/train_model.py
+++ task1/src/models/train_model.py
@@ -1,6 +1,28 @@
 from tqdm import tqdm
 import os, time, evaluate, torch
 from torch.utils.tensorboard import SummaryWriter
+
+def eval(model, tokenizer, valid_data_loader):
+    outputs = []
+    answers = []
+    model.eval()
+    for batch in tqdm(valid_data_loader):
+        input_token = batch[0].input_ids.squeeze().to('cuda')
+        input_mask = batch[0].attention_mask.squeeze().to('cuda')
+        ans_token = batch[1]
+
+        output = model.generate(
+            input_ids = input_token,
+            attention_mask = input_mask,
+            max_length=128,
+            early_stopping=True)
+        outputs.append(tokenizer.decode(output[0], skip_special_tokens=True))
+        answers.append(ans_token)
+    rouge = evaluate.load('rouge')
+    results = rouge.compute(predictions=outputs,references=answers)
+    return results['rougeL']
+
+
 def train(model, tokenizer, steps, learning_rate, train_data_loader, valid_data_loader):
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     loss_function = torch.nn.CrossEntropyLoss()
@@ -48,25 +70,6 @@ def train(model, tokenizer, steps, learning_rate, train_data_loader, valid_data_
 
     writer.close()
 
-def eval(model, tokenizer, valid_data_loader):
-    outputs = []
-    answers = []
-    model.eval()
-    for batch in tqdm(valid_data_loader):
-        input_token = batch[0].input_ids.squeeze().to('cuda')
-        input_mask = batch[0].attention_mask.squeeze().to('cuda')
-        ans_token = batch[1].squeeze()
-
-        output = model.generate(
-            input_ids = input_token,
-            attention_mask = input_mask,
-            max_length=128,
-            early_stopping=True)
-        outputs.append(tokenizer.decode(output[0], skip_special_tokens=True))
-        answers.append(ans_token)
-    rouge = evaluate.load('rouge')
-    results = rouge.compute(predictions=outputs,references=answers)
-    return results['rougeL']
 
 def epoch_time(start_time, end_time):
     elapsed_time = end_time - start_time

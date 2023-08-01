7bc382925715bb4440be1ee8847d494f9cf3c3cb
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-08-01 14:00:37 +0700

    Update task 2

--- task2/prepare_data.py
+++ task2/prepare_data.py
@@ -1,27 +1,33 @@
-# TODO 1: chunk data
+# TODO 1: chunk data and store data
 from datasets import load_dataset, load_from_disk
 from tqdm import tqdm
 import psycopg2
+from langchain.text_splitter import CharacterTextSplitter
+from preprocessing import reranking, tokening
+from pgvector.psycopg2 import register_vector
 
 wikis = load_from_disk('data')
 wikis = wikis['train']
 
 # create table
 def create_table(table_name):
+    conn = None
     try: 
         conn = psycopg2.connect(
             host='localhost',
             port="5432",
             dbname="postgres",
             user="postgres",
-            password=1)
+            password="1")
         cur = conn.cursor()
         # Enable the pgvector extension
         cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
         # Create database
         cur.execute(
-            # f"CREATE TABLE {table_name} (id SERIAL, chunk_text TEXT, embedding vector);")
-            f"CREATE TABLE {table_name} (id SERIAL PRIMARY KEY, chunk_text TEXT, embedding vector);")
+            f"DROP TABLE IF EXISTS {table_name}")
+        cur.execute(
+            f"CREATE TABLE {table_name} (id BIGSERIAL PRIMARY KEY, chunk_text TEXT, embedding vector);")
+        conn.commit()
         print(f"Database {table_name} create successfully")
     except psycopg2.OperationalError as e:
         print(f"Error: {e}")
@@ -30,37 +36,35 @@ def create_table(table_name):
             conn.close()
 
 def add_data(table_name):
+    conn = None
     try:
         text_splitter = CharacterTextSplitter(        
-            separator = ".\n\n",
-            chunk_size = 100,
+            separator = "\n\n\n",
+            chunk_size = 200,
             chunk_overlap  = 50,
-            # length_function = len,
             )
         conn = psycopg2.connect(
             host='localhost',
             port="5432",
             dbname="postgres",
             user="postgres",
-            password=1)
+            password="1")
         cur = conn.cursor()
+        # register_vector(cur)
         for wiki in tqdm(wikis):
             id = wiki['id']
             text = wiki['text']
             texts = text_splitter.create_documents([text])
-            table_name = 'wikipedia'
             command = f"INSERT INTO {table_name} (chunk_text, embedding) VALUES (%s, %s);"
             for i in range(len(texts)):
-                input_token = tokening(texts[i].page_content, 512).input_ids.squeeze(0)
+                input_token = tokening(texts[i].page_content, 512).input_ids.squeeze(0).cpu().detach().numpy().tolist()
                 cur.execute(command, (texts[i].page_content, input_token))
+        conn.commit()
     except psycopg2.OperationalError as e:
         print(f"Error: {e}")
     finally:
         if conn is not None:
             conn.close()
 
-# create database wikipedia
 create_table("wikipedia")
-# # disconect to the database
-# if conn is not None:
-#     conn.close()
+add_data("wikipedia")
